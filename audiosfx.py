526047d8d5faf34fdb958b77448df8b43ef459d1
Shayan Gheidi <67720747+amirblaese@users.noreply.github.com>
2022-04-25 22:39:50 -0700

    Updated with convolution reverb (faster)

--- audiosfx.py
+++ audiosfx.py
@@ -500,27 +500,29 @@ def slow(filename,p=10,wout=True):
   
 
 def conv_reverb(filename,ir="vocalduo.wav",wet=0.5,wout=True):  
-    start=time.time()
-    n, data, data_dB,sr,ch=inputwav(filename)
-    n_IR, data_IR, data_dB_IR,sr_IR,ch_IR=inputwav(ir)
-    print('Convolving impulse response with track...')
-    convolution = convolve(n,n_IR,mode='full')
-    for i in range(ch+1): #normalize the song
-      convolution[:,i]=convolution[:,i]/max(convolution[:,i])
-    song_pad = np.zeros(convolution[:,0:2].shape)
-    for i in range(ch):
-      song_pad[0:len(song)]=song
-    print('Mixing...')
-    final = wet*song_pad+(1-wet)*convolution[:,0:2]
-    if wout==True:
-        print('Exporting...')
-        sf.write(filename[0:len(filename)-4]+'_verbed.wav',data_verb,sr,'PCM_16')
-    print('Done!')
-    end=time.time()
-    elapsed=int(1000*(end-start))
-    print('...............................')
-    print('Completed in '+str(elapsed)+' milliseconds.')
-    return data_verb
+  start=time.time()
+  n, data, data_dB,sr,ch=inputwav(filename)
+  n_IR, data_IR, data_dB_IR,sr_IR,ch_IR=inputwav(ir)
+  print('Convolving impulse response with track...')
+  convolution = convolve(data,data_IR,mode='full')
+  print(ch)
+  for i in range(ch+1): #normalize the song
+    print(i)
+    convolution[:,i]=convolution[:,i]/max(convolution[:,i])
+  song_pad = np.zeros(convolution[:,0:2].shape)
+  for i in range(ch):
+    song_pad[0:len(data)]=data
+  print('Mixing...')
+  final = wet*song_pad+(1-wet)*convolution[:,0:2]
+  if wout==True:
+      print('Exporting...')
+      sf.write(filename[0:len(filename)-4]+'_verbed.wav',final,sr,'PCM_16')
+  print('Done!')
+  end=time.time()
+  elapsed=int(1000*(end-start))
+  print('...............................')
+  print('Completed in '+str(elapsed)+' milliseconds.')
+  return final
 
 
 
